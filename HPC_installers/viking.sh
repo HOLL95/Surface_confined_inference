d1800a159e8216df4347af971b1ce546ecf57fb1
Henry LL <henry.lloyd-laney@york.ac.uk>
2024-06-19 12:53:06 +0100

    updated viking install after update from ITSupport

--- HPC_installers/viking.sh
+++ HPC_installers/viking.sh
@@ -1,5 +1,5 @@
 module load Python/3.11.5-GCCcore-13.2.0
 module load pybind11/2.11.1-GCCcore-12.3.0
-module load SUNDIALS/2.7.0-foss-2021b
+module load SUNDIALS/6.6.0-foss-2023a
 alias python="python3.11"
 export IN_VIKING="true"
